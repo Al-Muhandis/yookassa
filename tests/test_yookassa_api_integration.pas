3c2dc68f5ac44790fbef9408d82520d6f226378d
Renat Suleymanov <mail@renat.su>
2025-08-01 01:49:50 +0400

    Update test_yookassa_api_integration.pas

--- tests/test_yookassa_api_integration.pas
+++ tests/test_yookassa_api_integration.pas
@@ -9,7 +9,12 @@ uses
   ;
 
 type
+
+  { TTestYooKassaIntegration }
+
   TTestYooKassaIntegration = class(TTestCase)
+  private
+    procedure LoadConfig(aPayment: TYookassaPayment);
   published
     procedure TestCreatePayment_Sandbox;
     procedure TestCreatePaymentWithReceipt_Sandbox;
@@ -21,28 +26,26 @@ uses
   IniFiles
   ;
 
+procedure TTestYooKassaIntegration.LoadConfig(aPayment: TYookassaPayment);
+var
+  aIni: TIniFile;
+begin
+  aIni := TIniFile.Create(ExtractFilePath(ParamStr(0)) + 'config.ini');
+  try
+    aPayment.ShopId     := aIni.ReadString('shop', 'ShopId', '');
+    aPayment.SecretKey  := aIni.ReadString('shop', 'SecretKey', '');
+    aPayment.Amount     := aIni.ReadFloat ('order', 'Amount', 10.00);
+    aPayment.Currency   := aIni.ReadString('order', 'Currency', 'RUB');
+    aPayment.Description:= aIni.ReadString('order', 'Description', 'Тест с чеком');
+    aPayment.ReturnUrl  := aIni.ReadString('order', 'ReturnUrl', 'https://example.com/return');
+  finally
+    aIni.Free;
+  end;
+end;
+
 procedure TTestYooKassaIntegration.TestCreatePayment_Sandbox;
 var
-  aShopId, aSecretKey, aCurrency, aDescription, aReturnUrl: string;
-  aAmount: Double;
   aConfirmationURL: String;
-
-  procedure LoadConfig;
-  var
-    aIni: TIniFile;
-  begin
-    aIni := TIniFile.Create(ExtractFilePath(ParamStr(0)) + 'config.ini');
-    try
-      aShopId     := aIni.ReadString('shop', 'ShopId', '');
-      aSecretKey  := aIni.ReadString('shop', 'SecretKey', '');
-      aAmount     := aIni.ReadFloat ('order', 'Amount', 10.00);
-      aCurrency   := aIni.ReadString('order', 'Currency', 'RUB');
-      aDescription:= aIni.ReadString('order', 'Description', 'Тест');
-      aReturnUrl  := aIni.ReadString('order', 'ReturnUrl', 'https://example.com/return');
-    finally
-      aIni.Free;
-    end;
-  end;
 
   procedure CreatePayment;
   var
@@ -50,13 +53,7 @@ var
   begin
     aPayment := TYookassaPayment.Create;
     try
-      aPayment.ShopId     := aShopId;
-      aPayment.SecretKey  := aSecretKey;
-      aPayment.Amount     := aAmount;
-      aPayment.Currency   := aCurrency;
-      aPayment.Description:= aDescription;
-      aPayment.ReturnUrl  := aReturnUrl;
-
+      LoadConfig(aPayment);
       aConfirmationURL := aPayment.CreatePayment;
     finally
       aPayment.Free;
@@ -64,7 +61,6 @@ var
   end;
 
 begin
-  LoadConfig;
   CreatePayment;
   AssertTrue('confirmation_url must be exists', Pos('http', aConfirmationURL) = 1);
 end;
@@ -72,26 +68,7 @@ end;
 // Интеграционный тест с отправкой receipt
 procedure TTestYooKassaIntegration.TestCreatePaymentWithReceipt_Sandbox;
 var
-  aShopId, aSecretKey, aCurrency, aDescription, aReturnUrl: string;
-  aAmount: Double;
   aConfirmationURL: String;
-
-  procedure LoadConfig;
-  var
-    aIni: TIniFile;
-  begin
-    aIni := TIniFile.Create(ExtractFilePath(ParamStr(0)) + 'config.ini');
-    try
-      aShopId     := aIni.ReadString('shop', 'ShopId', '');
-      aSecretKey  := aIni.ReadString('shop', 'SecretKey', '');
-      aAmount     := aIni.ReadFloat ('order', 'Amount', 10.00);
-      aCurrency   := aIni.ReadString('order', 'Currency', 'RUB');
-      aDescription:= aIni.ReadString('order', 'Description', 'Тест с чеком');
-      aReturnUrl  := aIni.ReadString('order', 'ReturnUrl', 'https://example.com/return');
-    finally
-      aIni.Free;
-    end;
-  end;
 
   procedure CreatePaymentWithReceipt;
   var
@@ -101,21 +78,15 @@ var
   begin
     aPayment := TYookassaPayment.Create;
     try
-      aPayment.ShopId     := aShopId;
-      aPayment.SecretKey  := aSecretKey;
-      aPayment.Amount     := aAmount;
-      aPayment.Currency   := aCurrency;
-      aPayment.Description:= aDescription;
-      aPayment.ReturnUrl  := aReturnUrl;
-
+      LoadConfig(aPayment);
       aReceipt := TYookassaReceipt.Create;
       aReceipt.CustomerEmail := 'user@example.com';
 
       aItem := TYookassaReceiptItem.Create;
       aItem.Description := 'Тестовый товар';
       aItem.Quantity := 1;
-      aItem.AmountValue := aAmount;
-      aItem.AmountCurrency := aCurrency;
+      aItem.AmountValue := aPayment.Amount;
+      aItem.AmountCurrency := aPayment.Currency;
       aItem.VatCode := 1;
       aItem.PaymentMode := 'full_prepayment';
       aItem.PaymentSubject := 'commodity';
@@ -131,7 +102,6 @@ var
   end;
 
 begin
-  LoadConfig;
   CreatePaymentWithReceipt;
   AssertTrue('confirmation_url must be exists', Pos('http', aConfirmationURL) = 1);
 end;
